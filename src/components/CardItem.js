224e8442c52fa142e2c9960d0adecbd397eb6a7d
skyi9 <88197965+skyi9@users.noreply.github.com>
2022-10-31 23:08:04 +0530

    second commit

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -14,11 +14,11 @@ const CardItem = (props) => {
                     </div>
                     <div className='p-4'>
                         <div className='d-flex flex-row justify-content-between'>
-                            <h5 className="card-title btn-text ">{props.title}</h5>
-                            <p style={{ backgroundColor: '#20AFEC', color: '#2608DF', borderRadius: '15px', padding: '0 1rem' }} htmlFor="">{props.option}</p>
+                            <h5 className="card-title btn-text ">Talk Show</h5>
+                            <p style={{ backgroundColor: '#20AFEC', color: '#2608DF', borderRadius: '15px', padding: '0 1rem' }} htmlFor="">Online</p>
                         </div>
-                        <p className="card-text"> <img className='me-2' src={calender} alt="" />{props.date}</p>
-                        <a href="/" style={{ textDecoration: 'none' }} className="link-primary"> <img className='me-2' src={network} alt="" />{props.link}</a>
+                        <p className="card-text"> <img className='me-2' src={calender} alt="" /> 29th Oct 2022 , 9.15 pm</p>
+                        <a href="/" style={{ textDecoration: 'none' }} className="link-primary"> <img className='me-2' src={network} alt="" />https://meet.google.co......</a>
                     </div>
                     <div className='d-flex justify-content-end px-4 mb-4'>
                         <img style={{ width: '.8rem' }} src={edit} alt="" />
